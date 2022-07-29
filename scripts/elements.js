c2d06716b293f2e668ca5c1425c93a2dbe483ac5
Jackson Alves <jack182@outlook.com.br>
2022-07-29 16:55:07 -0300

    <Fix/Style> some fix regarding style positioning of elements for mobile version

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,10 +1,17 @@
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('.close-menu');
 
+const cartIcon = document.querySelector('#icon-cart');
+const cartInfo = document.querySelector('.cart-info');
+const cartProduct = document.querySelector('.cart-product');
+
 const menuIcon = document.querySelector('.header-list');
 
 export{
   menu,
   menuIcon,
-  closeMenu
+  closeMenu,
+  cartIcon,
+  cartInfo,
+  cartProduct
 }
--- scripts/events.js
+++ scripts/events.js
@@ -2,6 +2,9 @@ import{
   menu,
   menuIcon,
   closeMenu,
+  cartIcon,
+  cartInfo,
+  cartProduct
 } from "./elements.js"
 
 export default function(){
@@ -21,6 +24,16 @@ export default function(){
     menuIcon.style.display = "none";
   })
 
+  cartIcon.addEventListener('click', function(){
+    var quantCart = document.querySelector('.quant-cart-notification');
+    
+    checkCart(quantCart);
+  })
+
+  cartInfo.addEventListener('mouseout', function(){
+    cartInfo.classList.add('hide');
+  })
+
   function resizeCheck(){
     var widthOut = window.innerWidth;
     if((widthOut > 625 )&&(menuIcon.style.display == "none")){
@@ -30,6 +43,17 @@ export default function(){
       menuIcon.style.display = "none";
     }
   }
+
+  function checkCart(quantCart){
+    if(quantCart.textContent == 0){
+      cartProduct.innerHTML = "<div class='empty-cart'>Your cart is empty</div>";
+      cartInfo.classList.remove('hide');
+    }
+    else{
+      cartInfo.classList.remove('hide');
+    }
+  }
+
   window.addEventListener('resize', resizeCheck);
 
   
